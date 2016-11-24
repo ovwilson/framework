3e01c1eef6baae1f81e57cbf4c8d6aa2be86cff0
landough <orlando_wilson@msn.com>
2016-11-24 15:47:57 -0500

    router updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from "@angular/router";
 
 export const APPRROUTES: Routes = [
     //{ path: '', redirectTo: '/remote', pathMatch: 'full'  },
-    { path: "", loadChildren: "app/dashboard/dashboard.module#DashboardModule" },
-    { path: "**", loadChildren: "app/dashboard/dashboard.module#DashboardModule" }
+    { path: "", loadChildren: "./dashboard/dashboard.module#DashboardModule" },
+    { path: "**", loadChildren: "./dashboard/dashboard.module#DashboardModule" }
 ];
