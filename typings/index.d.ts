36b52f551020cee224003b9437effe595f92d461
landough <orlando_wilson@msn.com>
2016-12-07 15:26:01 -0500

    chai typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -3,3 +3,4 @@
 /// <reference path="globals/jquery/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/webpack/index.d.ts" />
+/// <reference path="modules/chai/index.d.ts" />
